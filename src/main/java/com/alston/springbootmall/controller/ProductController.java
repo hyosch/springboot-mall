fb7c71eb3acb6b1e6c4e817cb2e5a7151c59ee29
Gondry <apple94021@gmail.com>
2023-03-19 13:39:05 +0800

    Add Product Function - deleteProduct

--- src/main/java/com/alston/springbootmall/controller/ProductController.java
+++ src/main/java/com/alston/springbootmall/controller/ProductController.java
@@ -53,4 +53,11 @@ public class ProductController {
 
     }
 
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProduct (@PathVariable Integer productId) {
+        productService.deleteProduct(productId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 }
