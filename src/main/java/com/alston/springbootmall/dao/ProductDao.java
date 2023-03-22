85fec2d984b00627ce7c3859200a6072021ee97d
Gondry <apple94021@gmail.com>
2023-03-22 13:23:12 +0800

    UPDATE Order - createOrder (check if user exists, stock)

--- src/main/java/com/alston/springbootmall/dao/ProductDao.java
+++ src/main/java/com/alston/springbootmall/dao/ProductDao.java
@@ -1,6 +1,5 @@
 package com.alston.springbootmall.dao;
 
-import com.alston.springbootmall.constant.ProductCategory;
 import com.alston.springbootmall.dto.ProductQueryParams;
 import com.alston.springbootmall.dto.ProductRequest;
 import com.alston.springbootmall.model.Product;
@@ -18,6 +17,8 @@ public interface ProductDao {
     Integer createProduct(ProductRequest productRequest);
 
     void updateProduct(Integer productId, ProductRequest productRequest);
+
+    void updateStock(Integer productId, Integer stock);
 
     void deleteProduct(Integer productId);
 
--- src/main/java/com/alston/springbootmall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/alston/springbootmall/service/impl/OrderServiceImpl.java
@@ -2,15 +2,21 @@ package com.alston.springbootmall.service.impl;
 
 import com.alston.springbootmall.dao.OrderDao;
 import com.alston.springbootmall.dao.ProductDao;
+import com.alston.springbootmall.dao.UserDao;
 import com.alston.springbootmall.dto.BuyItem;
 import com.alston.springbootmall.dto.CreateOrderRequest;
 import com.alston.springbootmall.model.Order;
 import com.alston.springbootmall.model.OrderItem;
 import com.alston.springbootmall.model.Product;
+import com.alston.springbootmall.model.User;
 import com.alston.springbootmall.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +24,17 @@ import java.util.List;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+
     @Autowired
     private OrderDao orderDao;
 
     @Autowired
     private ProductDao productDao;
+
+    @Autowired
+    private UserDao userDao;
+
+    private final static Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
 
     @Override
     public Order getOrderById(Integer orderId) {
@@ -38,12 +50,32 @@ public class OrderServiceImpl implements OrderService {
     @Transactional
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+        // 檢查用戶是否存在
+        User user = userDao.getUserById(userId);
+
+        if (user == null) {
+            log.warn("該用戶 {} 不存在", userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
 
         int totalAmount = 0;
         List<OrderItem> orderItemList = new ArrayList<>();
 
         for (BuyItem buyItem : createOrderRequest.getBuyItemList()) {
             Product product = productDao.getProductById(buyItem.getProductId());
+
+        //  清點庫存
+            if (product.getStock() <= 0) {
+                log.warn("商品 {} 無庫存", product.getProductName());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+            if (product.getStock() < buyItem.getQuantity()) {
+                log.warn("商品 {} 庫存不足, 尚餘 {}, 欲購買數量 {}",
+                        product.getProductName(), product.getStock(), buyItem.getQuantity());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+            productDao.updateStock(product.getProductId(), product.getStock() - buyItem.getQuantity());
 
         //  計算價錢
             int amount = buyItem.getQuantity() * product.getPrice();
